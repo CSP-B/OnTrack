b4def205b59847b642e9bdc5f6df7a946989651e
Jorden Whitefield <jorden.whitefield@googlemail.com>
2014-08-07 14:41:01 +0100

    Updated CTRL to new DSL

---
+++ Bjoerner_Generator/output/07.08.14_14.23.24/CTRL.csp.pl
@@ -0,0 +1,4 @@
+:- dynamic parserVersionNum/1, parserVersionStr/1, parseResult/5.
+:- dynamic module/4.
+'parserVersionStr'('0.5.0.0').
+'parseResult'('parseError','\xa\unexpected \x27\datatype\x27\\xa\expecting identifier',28,17,1218).
